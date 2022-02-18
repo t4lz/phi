bd439cd85f428f2b635efb678da4cdcdb26786c0
Tal Zwick <tal.zwick@tum.de>
2022-02-18 15:23:32 +0100

    Start pass

---
+++ phi_lib/PhiPass.hpp
@@ -0,0 +1,20 @@
+#ifndef PHI_PASS_H
+#define PHI_PASS_H
+
+#include <ir/bits.h>
+#include <pass.h>
+#include <wasm.h>
+
+//---------------------------------------------------------------------------
+namespace phi {
+//---------------------------------------------------------------------------
+using namespace wasm;
+
+//struct PhiPass : public WalkerPass<PostWalker<PhiPass>> {
+//};
+
+//---------------------------------------------------------------------------
+}   // namespace phi
+//---------------------------------------------------------------------------
+
+#endif //PHI_PASS_H
